c9b51f7953a8706aa882cfc686764bc50b4de042
Jason Veysey <38572989+jwveysey@users.noreply.github.com>
2019-05-02 13:53:08 -0400

    Updated this test case for v.1.0.0-alpha.

--- test/test_cplex.jl
+++ test/test_cplex.jl
@@ -1,8 +1,8 @@
 #=
-    NEMO: Next-generation Energy Modeling system for Optimization.
+    |nemo: Next-generation Energy Modeling system for Optimization.
     https://github.com/sei-international/NemoMod.jl
 
-    Copyright © 2018: Stockholm Environment Institute U.S.
+    Copyright © 2019: Stockholm Environment Institute U.S.
 
 	File description: A test of NemoMod package using CPLEX solver. This file is provided for users wishing
         to native compile NEMO with support for CPLEX.
@@ -14,60 +14,38 @@ using Test, SQLite, DataFrames, JuMP, CPLEX
 const TOL = 1e-4  # Default tolerance for isapprox() comparisons
 
 @testset "Solving a scenario" begin
-    @testset "Solving UTOPIA with CPLEX" begin
-        dbfile = joinpath(@__DIR__, "utopia.sqlite")
+    @testset "Solving storage_test with CPLEX" begin
+        dbfile = joinpath(@__DIR__, "storage_test.sqlite")
 
         NemoMod.calculatescenario(dbfile; jumpmodel = JuMP.Model(solver = CplexSolver()))
 
         db = SQLite.DB(dbfile)
         testqry = SQLite.query(db, "select * from vtotaldiscountedcost")
 
-        @test testqry[1,:y] == "1990"
-        @test testqry[2,:y] == "1991"
-        @test testqry[3,:y] == "1992"
-        @test testqry[4,:y] == "1993"
-        @test testqry[5,:y] == "1994"
-        @test testqry[6,:y] == "1995"
-        @test testqry[7,:y] == "1996"
-        @test testqry[8,:y] == "1997"
-        @test testqry[9,:y] == "1998"
-        @test testqry[10,:y] == "1999"
-        @test testqry[11,:y] == "2000"
-        @test testqry[12,:y] == "2001"
-        @test testqry[13,:y] == "2002"
-        @test testqry[14,:y] == "2003"
-        @test testqry[15,:y] == "2004"
-        @test testqry[16,:y] == "2005"
-        @test testqry[17,:y] == "2006"
-        @test testqry[18,:y] == "2007"
-        @test testqry[19,:y] == "2008"
-        @test testqry[20,:y] == "2009"
-        @test testqry[21,:y] == "2010"
+        @test testqry[1,:y] == "2020"
+        @test testqry[2,:y] == "2021"
+        @test testqry[3,:y] == "2022"
+        @test testqry[4,:y] == "2023"
+        @test testqry[5,:y] == "2024"
+        @test testqry[6,:y] == "2025"
+        @test testqry[7,:y] == "2026"
+        @test testqry[8,:y] == "2027"
+        @test testqry[9,:y] == "2028"
+        @test testqry[10,:y] == "2029"
 
-        @test isapprox(testqry[1,:val], 7078.726833; atol=TOL)
-        @test isapprox(testqry[2,:val], 1376.094496; atol=TOL)
-        @test isapprox(testqry[3,:val], 1424.622928; atol=TOL)
-        @test isapprox(testqry[4,:val], 1299.845856; atol=TOL)
-        @test isapprox(testqry[5,:val], 1341.383291; atol=TOL)
-        @test isapprox(testqry[6,:val], 1214.807617; atol=TOL)
-        @test isapprox(testqry[7,:val], 1196.944055; atol=TOL)
-        @test isapprox(testqry[8,:val], 1207.676216; atol=TOL)
-        @test isapprox(testqry[9,:val], 1098.252384; atol=TOL)
-        @test isapprox(testqry[10,:val], 1117.595158; atol=TOL)
-        @test isapprox(testqry[11,:val], 1047.769294; atol=TOL)
-        @test isapprox(testqry[12,:val], 1125.578368; atol=TOL)
-        @test isapprox(testqry[13,:val], 1047.590383; atol=TOL)
-        @test isapprox(testqry[14,:val], 1023.135753; atol=TOL)
-        @test isapprox(testqry[15,:val], 937.081597; atol=TOL)
-        @test isapprox(testqry[16,:val], 2029.877355; atol=TOL)
-        @test isapprox(testqry[17,:val], 885.294875; atol=TOL)
-        @test isapprox(testqry[18,:val], 838.923596; atol=TOL)
-        @test isapprox(testqry[19,:val], 777.346299; atol=TOL)
-        @test isapprox(testqry[20,:val], 718.539524; atol=TOL)
-        @test isapprox(testqry[21,:val], 659.775231; atol=TOL)
+        @test isapprox(testqry[1,:val], 3845.15703404259; atol=TOL)
+        @test isapprox(testqry[2,:val], 146.55227050539; atol=TOL)
+        @test isapprox(testqry[3,:val], 139.57362837926; atol=TOL)
+        @test isapprox(testqry[4,:val], 132.927266053843; atol=TOL)
+        @test isapprox(testqry[5,:val], 126.597396376304; atol=TOL)
+        @test isapprox(testqry[6,:val], 120.568948487497; atol=TOL)
+        @test isapprox(testqry[7,:val], 114.827569988092; atol=TOL)
+        @test isapprox(testqry[8,:val], 109.35959046485; atol=TOL)
+        @test isapprox(testqry[9,:val], 104.151990918904; atol=TOL)
+        @test isapprox(testqry[10,:val], 99.1923723037184; atol=TOL)
 
         # Delete test results and re-compact test database
         NemoMod.dropresulttables(db)
         testqry = SQLite.query(db, "VACUUM")
-    end  # "Solving UTOPIA with CPLEX"
+    end  # "Solving storage_test with CPLEX"
 end  # @testset "Solving a scenario"
