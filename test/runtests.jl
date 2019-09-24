1d88ff390596b7e24fbfa3f15496091e06ece680
Jason Veysey <jason.veysey@sei-us.org>
2019-09-24 10:50:00 -0400

    Updated test cases

--- test/runtests.jl
+++ test/runtests.jl
@@ -33,7 +33,69 @@ end  # delete_dbfile(path::String)
     @testset "Solving storage_test with GLPK" begin
         dbfile = joinpath(@__DIR__, "storage_test.sqlite")
 
-        NemoMod.calculatescenario(dbfile)  # GLPK is default solver
+        # Test with default outputs
+        NemoMod.calculatescenario(dbfile; quiet = false)  # GLPK is default solver
+
+        db = SQLite.DB(dbfile)
+        testqry = SQLite.query(db, "select * from vtotaldiscountedcost")
+
+        @test testqry[1,:y] == "2020"
+        @test testqry[2,:y] == "2021"
+        @test testqry[3,:y] == "2022"
+        @test testqry[4,:y] == "2023"
+        @test testqry[5,:y] == "2024"
+        @test testqry[6,:y] == "2025"
+        @test testqry[7,:y] == "2026"
+        @test testqry[8,:y] == "2027"
+        @test testqry[9,:y] == "2028"
+        @test testqry[10,:y] == "2029"
+
+        @test isapprox(testqry[1,:val], 3845.15711985937; atol=TOL)
+        @test isapprox(testqry[2,:val], 146.552326874185; atol=TOL)
+        @test isapprox(testqry[3,:val], 139.573639845721; atol=TOL)
+        @test isapprox(testqry[4,:val], 132.927276043543; atol=TOL)
+        @test isapprox(testqry[5,:val], 126.597405755756; atol=TOL)
+        @test isapprox(testqry[6,:val], 120.568957862625; atol=TOL)
+        @test isapprox(testqry[7,:val], 114.827578916785; atol=TOL)
+        @test isapprox(testqry[8,:val], 109.359598968367; atol=TOL)
+        @test isapprox(testqry[9,:val], 104.151999017492; atol=TOL)
+        @test isapprox(testqry[10,:val], 99.1923800166593; atol=TOL)
+
+        # Test with optional outputs
+        NemoMod.calculatescenario(dbfile; varstosave =
+            "vrateofproductionbytechnologybymode, vrateofusebytechnologybymode, vrateofdemand, vproductionbytechnology, vtotaltechnologyannualactivity, "
+            * "vtotaltechnologymodelperiodactivity, vusebytechnology, vmodelperiodcostbyregion, vannualtechnologyemissionpenaltybyemission, "
+            * "vtotaldiscountedcost", quiet = false)
+
+        db = SQLite.DB(dbfile)
+        testqry = SQLite.query(db, "select * from vtotaldiscountedcost")
+
+        @test testqry[1,:y] == "2020"
+        @test testqry[2,:y] == "2021"
+        @test testqry[3,:y] == "2022"
+        @test testqry[4,:y] == "2023"
+        @test testqry[5,:y] == "2024"
+        @test testqry[6,:y] == "2025"
+        @test testqry[7,:y] == "2026"
+        @test testqry[8,:y] == "2027"
+        @test testqry[9,:y] == "2028"
+        @test testqry[10,:y] == "2029"
+
+        @test isapprox(testqry[1,:val], 3845.15711985937; atol=TOL)
+        @test isapprox(testqry[2,:val], 146.552326874185; atol=TOL)
+        @test isapprox(testqry[3,:val], 139.573639845721; atol=TOL)
+        @test isapprox(testqry[4,:val], 132.927276043543; atol=TOL)
+        @test isapprox(testqry[5,:val], 126.597405755756; atol=TOL)
+        @test isapprox(testqry[6,:val], 120.568957862625; atol=TOL)
+        @test isapprox(testqry[7,:val], 114.827578916785; atol=TOL)
+        @test isapprox(testqry[8,:val], 109.359598968367; atol=TOL)
+        @test isapprox(testqry[9,:val], 104.151999017492; atol=TOL)
+        @test isapprox(testqry[10,:val], 99.1923800166593; atol=TOL)
+
+        # Test with restrictvars
+        NemoMod.calculatescenario(dbfile; varstosave =
+            "vrateofproductionbytechnologybymode, vrateofusebytechnologybymode, vproductionbytechnology, vusebytechnology, "
+            * "vtotaldiscountedcost", restrictvars = true, targetprocs = Array{Int, 1}([1]), quiet = false)
 
         db = SQLite.DB(dbfile)
         testqry = SQLite.query(db, "select * from vtotaldiscountedcost")
@@ -68,7 +130,73 @@ end  # delete_dbfile(path::String)
     @testset "Solving storage_test with Cbc" begin
         dbfile = joinpath(@__DIR__, "storage_test.sqlite")
 
-        NemoMod.calculatescenario(dbfile; jumpmodel = Model(solver = CbcSolver(logLevel=1, presolve="on")))
+        # Test with default outputs
+        NemoMod.calculatescenario(dbfile; jumpmodel = Model(solver = CbcSolver(logLevel=1, presolve="on")),
+            quiet = false)
+
+        db = SQLite.DB(dbfile)
+        testqry = SQLite.query(db, "select * from vtotaldiscountedcost")
+
+        @test testqry[1,:y] == "2020"
+        @test testqry[2,:y] == "2021"
+        @test testqry[3,:y] == "2022"
+        @test testqry[4,:y] == "2023"
+        @test testqry[5,:y] == "2024"
+        @test testqry[6,:y] == "2025"
+        @test testqry[7,:y] == "2026"
+        @test testqry[8,:y] == "2027"
+        @test testqry[9,:y] == "2028"
+        @test testqry[10,:y] == "2029"
+
+        @test isapprox(testqry[1,:val], 3845.15711985937; atol=TOL)
+        @test isapprox(testqry[2,:val], 146.552326874185; atol=TOL)
+        @test isapprox(testqry[3,:val], 139.573639845721; atol=TOL)
+        @test isapprox(testqry[4,:val], 132.927276043543; atol=TOL)
+        @test isapprox(testqry[5,:val], 126.597405755756; atol=TOL)
+        @test isapprox(testqry[6,:val], 120.568957862625; atol=TOL)
+        @test isapprox(testqry[7,:val], 114.827578916785; atol=TOL)
+        @test isapprox(testqry[8,:val], 109.359598968367; atol=TOL)
+        @test isapprox(testqry[9,:val], 104.151999017492; atol=TOL)
+        @test isapprox(testqry[10,:val], 99.1923800166593; atol=TOL)
+
+        # Test with optional outputs
+        NemoMod.calculatescenario(dbfile; jumpmodel = Model(solver = CbcSolver(logLevel=1, presolve="on")),
+            varstosave =
+                "vrateofproductionbytechnologybymode, vrateofusebytechnologybymode, vrateofdemand, vproductionbytechnology, vtotaltechnologyannualactivity, "
+                * "vtotaltechnologymodelperiodactivity, vusebytechnology, vmodelperiodcostbyregion, vannualtechnologyemissionpenaltybyemission, "
+                * "vtotaldiscountedcost",
+            quiet = false)
+
+        db = SQLite.DB(dbfile)
+        testqry = SQLite.query(db, "select * from vtotaldiscountedcost")
+
+        @test testqry[1,:y] == "2020"
+        @test testqry[2,:y] == "2021"
+        @test testqry[3,:y] == "2022"
+        @test testqry[4,:y] == "2023"
+        @test testqry[5,:y] == "2024"
+        @test testqry[6,:y] == "2025"
+        @test testqry[7,:y] == "2026"
+        @test testqry[8,:y] == "2027"
+        @test testqry[9,:y] == "2028"
+        @test testqry[10,:y] == "2029"
+
+        @test isapprox(testqry[1,:val], 3845.15711985937; atol=TOL)
+        @test isapprox(testqry[2,:val], 146.552326874185; atol=TOL)
+        @test isapprox(testqry[3,:val], 139.573639845721; atol=TOL)
+        @test isapprox(testqry[4,:val], 132.927276043543; atol=TOL)
+        @test isapprox(testqry[5,:val], 126.597405755756; atol=TOL)
+        @test isapprox(testqry[6,:val], 120.568957862625; atol=TOL)
+        @test isapprox(testqry[7,:val], 114.827578916785; atol=TOL)
+        @test isapprox(testqry[8,:val], 109.359598968367; atol=TOL)
+        @test isapprox(testqry[9,:val], 104.151999017492; atol=TOL)
+        @test isapprox(testqry[10,:val], 99.1923800166593; atol=TOL)
+
+        # Test with restrictvars
+        NemoMod.calculatescenario(dbfile; jumpmodel = Model(solver = CbcSolver(logLevel=1, presolve="on")),
+            varstosave = "vrateofproductionbytechnologybymode, vrateofusebytechnologybymode, vproductionbytechnology, vusebytechnology, "
+                * "vtotaldiscountedcost",
+            restrictvars = true, targetprocs = Array{Int, 1}([1]), quiet = false)
 
         db = SQLite.DB(dbfile)
         testqry = SQLite.query(db, "select * from vtotaldiscountedcost")
--- test/test_cplex.jl
+++ test/test_cplex.jl
@@ -17,7 +17,72 @@ const TOL = 1e-4  # Default tolerance for isapprox() comparisons
     @testset "Solving storage_test with CPLEX" begin
         dbfile = joinpath(@__DIR__, "storage_test.sqlite")
 
-        NemoMod.calculatescenario(dbfile; jumpmodel = JuMP.Model(solver = CplexSolver()))
+        # Test with default outputs
+        NemoMod.calculatescenario(dbfile; jumpmodel = JuMP.Model(solver = CplexSolver()), quiet = false)
+
+        db = SQLite.DB(dbfile)
+        testqry = SQLite.query(db, "select * from vtotaldiscountedcost")
+
+        @test testqry[1,:y] == "2020"
+        @test testqry[2,:y] == "2021"
+        @test testqry[3,:y] == "2022"
+        @test testqry[4,:y] == "2023"
+        @test testqry[5,:y] == "2024"
+        @test testqry[6,:y] == "2025"
+        @test testqry[7,:y] == "2026"
+        @test testqry[8,:y] == "2027"
+        @test testqry[9,:y] == "2028"
+        @test testqry[10,:y] == "2029"
+
+        @test isapprox(testqry[1,:val], 3845.15703404259; atol=TOL)
+        @test isapprox(testqry[2,:val], 146.55227050539; atol=TOL)
+        @test isapprox(testqry[3,:val], 139.57362837926; atol=TOL)
+        @test isapprox(testqry[4,:val], 132.927266053843; atol=TOL)
+        @test isapprox(testqry[5,:val], 126.597396376304; atol=TOL)
+        @test isapprox(testqry[6,:val], 120.568948487497; atol=TOL)
+        @test isapprox(testqry[7,:val], 114.827569988092; atol=TOL)
+        @test isapprox(testqry[8,:val], 109.35959046485; atol=TOL)
+        @test isapprox(testqry[9,:val], 104.151990918904; atol=TOL)
+        @test isapprox(testqry[10,:val], 99.1923723037184; atol=TOL)
+
+        # Test with optional outputs
+        NemoMod.calculatescenario(dbfile; jumpmodel = JuMP.Model(solver = CplexSolver()),
+            varstosave =
+                "vrateofproductionbytechnologybymode, vrateofusebytechnologybymode, vrateofdemand, vproductionbytechnology, vtotaltechnologyannualactivity, "
+                * "vtotaltechnologymodelperiodactivity, vusebytechnology, vmodelperiodcostbyregion, vannualtechnologyemissionpenaltybyemission, "
+                * "vtotaldiscountedcost",
+            quiet = false)
+
+        db = SQLite.DB(dbfile)
+        testqry = SQLite.query(db, "select * from vtotaldiscountedcost")
+
+        @test testqry[1,:y] == "2020"
+        @test testqry[2,:y] == "2021"
+        @test testqry[3,:y] == "2022"
+        @test testqry[4,:y] == "2023"
+        @test testqry[5,:y] == "2024"
+        @test testqry[6,:y] == "2025"
+        @test testqry[7,:y] == "2026"
+        @test testqry[8,:y] == "2027"
+        @test testqry[9,:y] == "2028"
+        @test testqry[10,:y] == "2029"
+
+        @test isapprox(testqry[1,:val], 3845.15703404259; atol=TOL)
+        @test isapprox(testqry[2,:val], 146.55227050539; atol=TOL)
+        @test isapprox(testqry[3,:val], 139.57362837926; atol=TOL)
+        @test isapprox(testqry[4,:val], 132.927266053843; atol=TOL)
+        @test isapprox(testqry[5,:val], 126.597396376304; atol=TOL)
+        @test isapprox(testqry[6,:val], 120.568948487497; atol=TOL)
+        @test isapprox(testqry[7,:val], 114.827569988092; atol=TOL)
+        @test isapprox(testqry[8,:val], 109.35959046485; atol=TOL)
+        @test isapprox(testqry[9,:val], 104.151990918904; atol=TOL)
+        @test isapprox(testqry[10,:val], 99.1923723037184; atol=TOL)
+
+        # Test with restrictvars
+        NemoMod.calculatescenario(dbfile; jumpmodel = JuMP.Model(solver = CplexSolver()),
+            varstosave = "vrateofproductionbytechnologybymode, vrateofusebytechnologybymode, vproductionbytechnology, vusebytechnology, "
+                * "vtotaldiscountedcost",
+            restrictvars = true, targetprocs = Array{Int, 1}([1]), quiet = false)
 
         db = SQLite.DB(dbfile)
         testqry = SQLite.query(db, "select * from vtotaldiscountedcost")
