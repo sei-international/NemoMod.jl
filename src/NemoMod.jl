cb5b14fbedb6b08a39cfca270594dd351e9579e5
Jason Veysey <jason.veysey@sei-us.org>
2020-09-02 10:18:57 -0400

    Xpress work 2

--- src/NemoMod.jl
+++ src/NemoMod.jl
@@ -21,7 +21,7 @@ using JuMP, SQLite, DataFrames, Distributed, Dates, ConfParser
 using GLPKMathProgInterface, Cbc  # Open-source solvers
 
 # Proprietary solvers - enclosed in try blocks for users who aren't using NEMO installer
-try
+#= try
     using CPLEX
 catch
     # Just continue
@@ -45,6 +45,7 @@ catch e
     isa(e, InitError) && println("Xpress InitError")
     # Just continue
 end
+=#
 
 include("nemo_functions.jl")  # Core NEMO functions
 include("scenario_calculation.jl")  # Functions for calculating a scenario with NEMO
