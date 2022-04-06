e5fdfed6d10706fc63f98e2b5693f6867fa9e1a0
Jason Veysey <jason.veysey@sei-us.org>
2022-04-06 08:17:52 -0400

    Adjustments for Mosek 9.3

--- test/mosek_tests.jl
+++ test/mosek_tests.jl
@@ -15,17 +15,18 @@ catch e
 end
 
 # Tests will be skipped if MosekTools package is not installed.
+# Note: As of Mosek 9.3, Mosek does not seem to produce reliable results with NEMO unless forcemip=true
 if @isdefined MosekTools
     @info "Testing scenario solution with Mosek."
 
     @testset "Solving storage_test with Mosek" begin
         dbfile = joinpath(@__DIR__, "storage_test.sqlite")
-        # dbfile = "c:/temp/storage_test.sqlite"
+        #dbfile = "c:/temp/storage_test.sqlite"
         chmod(dbfile, 0o777)  # Make dbfile read-write. Necessary because after Julia 1.0, Pkg.add makes all package files read-only
 
         # Test with default outputs
         NemoMod.calculatescenario(dbfile; jumpmodel = JuMP.Model(Mosek.Optimizer),
-            restrictvars=false, quiet = false)
+            restrictvars=false, forcemip=true, quiet = false)
 
         db = SQLite.DB(dbfile)
 
@@ -61,7 +62,7 @@ if @isdefined MosekTools
                 "vrateofproductionbytechnologybymode, vrateofusebytechnologybymode, vrateofdemand, vproductionbytechnology, vtotaltechnologyannualactivity, "
                 * "vtotaltechnologymodelperiodactivity, vusebytechnology, vmodelperiodcostbyregion, vannualtechnologyemissionpenaltybyemission, "
                 * "vtotaldiscountedcost",
-            restrictvars=false, quiet = false)
+            restrictvars=false, forcemip=true, quiet = false)
 
         if !compilation
             testqry = SQLite.DBInterface.execute(db, "select * from vtotaldiscountedcost") |> DataFrame
@@ -93,7 +94,7 @@ if @isdefined MosekTools
         NemoMod.calculatescenario(dbfile; jumpmodel = JuMP.Model(Mosek.Optimizer),
             varstosave = "vrateofproductionbytechnologybymode, vrateofusebytechnologybymode, vproductionbytechnology, vusebytechnology, "
                 * "vtotaldiscountedcost",
-            restrictvars = true, quiet = false)
+            restrictvars = true, forcemip=true, quiet = false)
 
         if !compilation
             testqry = SQLite.DBInterface.execute(db, "select * from vtotaldiscountedcost") |> DataFrame
@@ -123,7 +124,7 @@ if @isdefined MosekTools
 
         # Test with storage net zero constraints
         SQLite.DBInterface.execute(db, "update STORAGE set netzeroyear = 1")
-        NemoMod.calculatescenario(dbfile; jumpmodel = JuMP.Model(Mosek.Optimizer), restrictvars=false)
+        NemoMod.calculatescenario(dbfile; jumpmodel = JuMP.Model(Mosek.Optimizer), restrictvars=false, forcemip=true)
 
         if !compilation
             testqry = SQLite.DBInterface.execute(db, "select * from vtotaldiscountedcost") |> DataFrame
@@ -154,7 +155,7 @@ if @isdefined MosekTools
         SQLite.DBInterface.execute(db, "update STORAGE set netzeroyear = 0")
 
         # Test with calcyears
-        NemoMod.calculatescenario(dbfile; jumpmodel = Model(Mosek.Optimizer), restrictvars=true,
+        NemoMod.calculatescenario(dbfile; jumpmodel = Model(Mosek.Optimizer), restrictvars=true, forcemip=true,
             calcyears=[2020,2029])
 
         if !compilation
@@ -169,7 +170,7 @@ if @isdefined MosekTools
 
         # Test MinimumUtilization
         SQLite.DBInterface.execute(db, "insert into MinimumUtilization select ROWID, '1', 'gas', val, 2025, 0.5 from TIMESLICE")
-        NemoMod.calculatescenario(dbfile; jumpmodel = Model(Mosek.Optimizer), varstosave="vproductionbytechnologyannual")
+        NemoMod.calculatescenario(dbfile; jumpmodel = Model(Mosek.Optimizer), varstosave="vproductionbytechnologyannual", forcemip=true)
 
         if !compilation
             testqry = SQLite.DBInterface.execute(db, "select * from vproductionbytechnologyannual where t = 'gas' and y = 2025") |> DataFrame
@@ -193,7 +194,7 @@ if @isdefined MosekTools
             varstosave =
                 "vdemandnn, vnewcapacity, vtotalcapacityannual, vproductionbytechnologyannual, vproductionnn, vusebytechnologyannual, vusenn, vtotaldiscountedcost, "
                 * "vtransmissionbuilt, vtransmissionexists, vtransmissionbyline, vtransmissionannual",
-            restrictvars=false, quiet = false)
+            restrictvars=false, forcemip=true, quiet = false)
 
         db = SQLite.DB(dbfile)
 
@@ -225,7 +226,7 @@ if @isdefined MosekTools
 
         # Test MinimumUtilization
         SQLite.DBInterface.execute(db, "insert into MinimumUtilization select ROWID, '1', 'gas', val, 2025, 0.2 from TIMESLICE")
-        NemoMod.calculatescenario(dbfile; jumpmodel = Model(Mosek.Optimizer), varstosave="vproductionbytechnologyannual", calcyears=[2020,2025,2029])
+        NemoMod.calculatescenario(dbfile; jumpmodel = Model(Mosek.Optimizer), varstosave="vproductionbytechnologyannual", calcyears=[2020,2025,2029], forcemip=true)
 
         if !compilation
             testqry = SQLite.DBInterface.execute(db, "select * from vproductionbytechnologyannual where t = 'gas' and y = 2025") |> DataFrame
@@ -239,7 +240,7 @@ if @isdefined MosekTools
         SQLite.DBInterface.execute(db, "insert into InterestRateStorage select rowid, 1, 'storage1', y.val, 0.05 from year y")
         SQLite.DBInterface.execute(db, "insert into InterestRateTechnology select rowid, 1, 'solar', y.val, 0.05 from year y")
         SQLite.DBInterface.execute(db, "update TransmissionLine set interestrate = 0.05 where id = 2")
-        NemoMod.calculatescenario(dbfile; jumpmodel = Model(Mosek.Optimizer), varstosave="vtotaldiscountedcost", calcyears=[2020,2025,2029])
+        NemoMod.calculatescenario(dbfile; jumpmodel = Model(Mosek.Optimizer), varstosave="vtotaldiscountedcost", calcyears=[2020,2025,2029], forcemip=true)
 
         if !compilation
             testqry = SQLite.DBInterface.execute(db, "select * from vtotaldiscountedcost") |> DataFrame
@@ -260,7 +261,7 @@ if @isdefined MosekTools
         # Test transshipment power flow
         SQLite.DBInterface.execute(db, "update TransmissionModelingEnabled set type = 3")
         SQLite.DBInterface.execute(db, "update TransmissionLine set efficiency = 1.0")
-        NemoMod.calculatescenario(dbfile; jumpmodel = Model(Mosek.Optimizer), varstosave="vtotaldiscountedcost", calcyears=[2020,2025,2029])
+        NemoMod.calculatescenario(dbfile; jumpmodel = Model(Mosek.Optimizer), varstosave="vtotaldiscountedcost", calcyears=[2020,2025,2029], forcemip=true)
 
         if !compilation
             testqry = SQLite.DBInterface.execute(db, "select * from vtotaldiscountedcost") |> DataFrame
@@ -288,7 +289,7 @@ if @isdefined MosekTools
         chmod(dbfile, 0o777)  # Make dbfile read-write. Necessary because after Julia 1.0, Pkg.add makes all package files read-only
 
         NemoMod.calculatescenario(dbfile; jumpmodel = JuMP.Model(Mosek.Optimizer),
-            restrictvars=false, quiet = false)
+            restrictvars=false, forcemip=true, quiet = false)
 
         db = SQLite.DB(dbfile)
 
